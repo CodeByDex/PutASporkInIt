e08ca31e30127e5e66ca75affbc27b65aed95990
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-04-29 17:47:06 -0400

    installed handlebars, created login.handlebars, and wrote router.get login within navigationRoutes

--- controllers/navigationRoutes.js
+++ controllers/navigationRoutes.js
@@ -1,3 +1,12 @@
 const router = require("express").Router();
 
+router.get('/login', (req, res) => {
+    // if (req.session.loggedIn) {
+    //   res.redirect('/');
+    //   return;
+    // }
+  
+    res.render('login');
+  });
+
 module.exports = router;

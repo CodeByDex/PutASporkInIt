5c703df9c9346418fe09521fb032f55b53f23c41
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-04-29 17:17:11 -0400

    adding helper class

---
+++ controllers/util.js
@@ -0,0 +1,87 @@
+module.exports = {
+    SafeGetAll: SafeGetAll,
+    SafeGetByID: SafeGetByID,
+    SafeCreate: SafeCreate,
+    SafeUpdate: SafeUpdate,
+    SafeDelete: SafeDelete
+}
+
+async function SafeRequest(res, cb) {
+    try {
+        await cb(res);
+    } catch (err) {
+        console.log(err);
+        let errCode = 500;
+        let errMsg = "Internal Error";
+
+        if (err instanceof ParamError) {
+            errCode = err.statusCode;
+            errMsg = err.message;
+        }
+
+        res.status(errCode).json(errMsg);
+    }
+}
+
+async function SafeGetAll(res, model, includes) {
+    SafeRequest(res, async (res) => {
+        const modelData = await model.findAll({
+            include: includes
+        });
+
+        res.json(modelData.map(x => x.get()));
+    })
+}
+
+async function SafeGetByID(ID, res, model, includes) {
+    SafeRequest(res, async (res) => {
+        const modelData = await getByID(ID, model, includes);
+
+        res.json(modelData.get());
+    })
+}
+
+async function SafeCreate(res, model, modelData) {
+    SafeRequest(res, async (res) => {
+        res.json(await model.create(modelData));
+    })
+}
+
+async function SafeUpdate(ID, res, model, modelData) {
+    SafeRequest(res, async (res) => {
+        let reqModRec = await getByID(ID, model, []);
+
+        reqModRec.set(modelData);
+
+        let upModRec = await reqModRec.save();
+
+        res.json(upModRec);
+    })
+}
+
+async function SafeDelete(ID, res, model) {
+    SafeRequest(res, async (res) => {
+        let reqMod = await getByID(ID, model, []);
+
+        let reqRes = await reqMod.destroy();
+
+        res.json(reqRes);
+    })
+}
+
+async function getByID(ID, model, includes) {
+    if (isNaN(ID)) {
+        throw new ParamError();
+    }
+
+    return await model.findByPk(ID, {
+        include: includes
+    });
+}
+
+class ParamError extends Error {
+    constructor() {
+        super("Invalid Param Value");
+        this.statusCode = 400;
+    }
+}

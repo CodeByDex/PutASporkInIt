2efe35bcf1b5c1a0c418abaa57eac5f5605c7301
Nathan Moon <moonman700@gmail.com>
2023-04-30 12:52:32 -0400

    added file structure for ingredient controller

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const userRoutes = require('./userRoutes');
+const ingredientRoutes = require('./ingredientRoutes');
 
 router.use('/users', userRoutes);
+router.use('/ingredients', ingredientRoutes);
 module.exports = router;
---
+++ controllers/api/ingredientRoutes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const {Ingredient} = require('../../models');
+const helper = require('../util')
+
+module.exports = router;

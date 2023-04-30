dfa601d457454c548b06e278898d52fc4491add1
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-04-30 16:18:24 -0400

    added UOM API

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,12 @@ const userRoutes = require('./userRoutes');
 const ingredientRoutes = require('./ingredientRoutes');
 const recipeRoutes = require('./recipeRoutes')
 const userPassword = require('./userPasswordRoutes')
+const uomRoutes = require("./UOM");
 
 router.use('/users', userRoutes);
 router.use('/ingredients', ingredientRoutes);
 router.use('/recipes', recipeRoutes);
 router.use('/userpassword', userPassword)
+router.use("/UOM", uomRoutes);
+
 module.exports = router;

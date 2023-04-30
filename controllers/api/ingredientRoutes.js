468763a82e25df4f91cae59c627ebb27c291cf42
Nathan Moon <moonman700@gmail.com>
2023-04-30 12:57:46 -0400

    added POST request to ingredient controller

--- controllers/api/ingredientRoutes.js
+++ controllers/api/ingredientRoutes.js
@@ -2,4 +2,10 @@ const router = require('express').Router();
 const {Ingredient} = require('../../models');
 const helper = require('../util')
 
+router.post('/', (req, res) => {
+    helper.SafeCreate(res, Ingredient, {
+    name: req.body.name,
+    defaultUOM: req.body.defaultUOM,
+    })
+})
 module.exports = router;

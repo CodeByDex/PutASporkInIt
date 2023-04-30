16635a1d03d20caf460b10912664930ccf501a0b
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-04-30 14:24:05 -0400

    added more conversion methods

--- util/Units.js
+++ util/Units.js
@@ -1,5 +1,30 @@
 const convert = require("convert-units");
 const qty = "qty";
+const excludedUOMs = [
+    // "mcg",
+    // "mg",
+    // "mt",
+    // "t",
+    // "m3",
+    // "c3",
+    "cl",
+    "dc",
+    "kl",
+    "m3",
+    "km3",
+    "krm",
+    "tsk",
+    "msk",
+    "kkp",
+    "glas",
+    "kanna",
+    "in3",
+    "ft3",
+    "yd3",
+    // "ns",
+    // "mu",
+    // "ms"
+];
 
 function GetAllIngredientUOMs() {
     let uom = [{
@@ -38,10 +63,29 @@ function ConvertValue(value, from, to) {
     };
 
     return convert(value).from(from).to(to);
+};
+
+function ConvertValueToBestMetric(value, from){
+    return ConvertValueToSystem(value, from, "metric");
+}
+
+function ConvertValueToBestImperial(value, from){
+    return ConvertValueToSystem(value, from, "imperial");
+}
+
+function ConvertValueToBest(value, from){
+    return convert(value).from(from).toBest({exclude: excludedUOMs});
+}
+
+function ConvertValueToSystem(value, from, system){
+    return convert(value).from(from).toBest({system: system, exclude: excludedUOMs});
 }
 
 module.exports = {
     GetAllIngredientUOMs,
     GetTimeUOMs,
-    ConvertValue
+    ConvertValue,
+    ConvertValueToBest,
+    ConvertValueToBestImperial,
+    ConvertValueToBestMetric
 }

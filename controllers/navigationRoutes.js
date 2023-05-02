9fecc27ac1c93c3dc5bc0d5d6b2fff0b7f7e971a
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-05-02 19:51:57 -0400

    implemented recipe route view model return

--- controllers/navigationRoutes.js
+++ controllers/navigationRoutes.js
@@ -1,42 +1,61 @@
 const router = require("express").Router();
-const { User, UserPassword, Recipe, Ingredient } = require('../models');
+const { User, UserPassword, Recipe, Ingredient, RecipeIngredient } = require('../models');
 
 
 // GET route for login page
 router.get('/login', (req, res) => {
-    // if (req.session.loggedIn) {
-    //   res.redirect('/');
-    //   return;
-    // }git
-  
-    res.render('login');
-  });
+  // if (req.session.loggedIn) {
+  //   res.redirect('/');
+  //   return;
+  // }git
 
-  // GET route for home page
-  router.get('/', async (req, res) => {
-    const recipes = await Recipe.findAll({})
-    // Pull in first three recipes in array
-    const topThreeRecipes = recipes.slice(0, 3)
-    res.render('home', { topRecipes: topThreeRecipes})
-  })
+  res.render('login');
+});
+
+// GET route for home page
+router.get('/', async (req, res) => {
+  const recipes = await Recipe.findAll({})
+  // Pull in first three recipes in array
+  const topThreeRecipes = recipes.slice(0, 3)
+  res.render('home', { topRecipes: topThreeRecipes })
+})
 
 
-  // GET route for dashboard page (user profile/account)
+// GET route for dashboard page (user profile/account)
 router.get('/dashboard', (req, res) => {
   res.render('dashboard')
 })
 
-  // GET route for recipe page
-  router.get('/recipe/:id', (req, res) => {
-    res.render('recipe');
+// GET route for recipe page
+router.get('/recipe/:id', async (req, res) => {
+  const recData = await Recipe.findByPk(req.params.id, {
+    include: {
+      model: RecipeIngredient,
+      include: Ingredient
+    }
+  });
+
+  let recipe = recData.get();
+  recipe.RecipeIngredients = recipe.RecipeIngredients.map(x => {
+    let recIng = x.get();
+    let ing = recIng.Ingredient.get();
+
+    return {
+      amount: recIng.amount,
+      UOM: recIng.UOM,
+      name: ing.name
+    };
   })
 
-  // GET route for browser page
-  router.get('/browse', async (req, res) => {
-    const recipes = await Recipe.findAll({})
-    //TODO: Implement UserRecipeFavorite get conditionally if the user is logged in
-    res.render('browse', { recipeContent: recipes});
-  })
+  res.render('recipe', recipe);
+})
+
+// GET route for browser page
+router.get('/browse', async (req, res) => {
+  const recipes = await Recipe.findAll({})
+  //TODO: Implement UserRecipeFavorite get conditionally if the user is logged in
+  res.render('browse', { recipeContent: recipes });
+})
 
 
 module.exports = router;

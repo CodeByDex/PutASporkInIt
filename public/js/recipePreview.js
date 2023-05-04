62824570121b7ff104f4d5a3ed3e15c92c0d808e
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-05-04 11:18:57 -0400

    added util helper in UOM.js file

--- public/js/recipePreview.js
+++ public/js/recipePreview.js
@@ -1,13 +1,30 @@
-
-const imageClicked = document.querySelector("img");
-
-// Determine which Recipe Preview Image User clicked
-
-window.addEventListener("click", )
+// Variable set of which Recipe Preview Image User clicked
+let imageClicked = document.querySelector("img");
 
 
-// Response should take user to the full page view of recipe corresponding to the recipe preview image
+imageClicked.addEventListener("click", async (event) => {
+   let recipeID ;
 
-router.get('/:id', (req, res) => {
-    helper.SafeGetByID(req.params.id, res, Recipe, [])
-})
+    // Response should take user to the full page view of recipe corresponding to the recipe preview image
+    const response = await fetch("./api/recipes", {
+        method: "GET",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({
+            id: id,
+        })        
+    });
+        if (response.ok) {
+      // Send a GET request to the server with the recipe ID as a parameter
+        window.location.href = `./api/recipes/${recipeId}`;
+        }
+        else {
+            // TODO add handling for bad response 
+            }   
+    })
+    
+} )
+
+
+

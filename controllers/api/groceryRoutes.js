cd9115ca0c355d6e898907e1ff86a86daf93c90c
Christian Smith <christiansmith@Christians-MBP.attlocal.net>
2023-05-03 21:04:02 -0400

    created groceryRoutes file

---
+++ controllers/api/groceryRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const {Grocery} = require('../../models');
+const helper = require('../util');
+
+router.post('/', (req, res) => {
+
+});
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+module.exports = router;

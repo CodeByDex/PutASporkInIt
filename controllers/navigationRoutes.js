7f6a81b0287c8eb929cf206572904d11b505e0d0
Zach Mutch <zkmutch@gmail.com>
2023-05-05 00:14:53 -0400

    removed a second helper util from nav routes, continued working on classes for dark/light mode

--- controllers/navigationRoutes.js
+++ controllers/navigationRoutes.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const helper = require('./util')
 const { User, UserPassword, Recipe, Ingredient, RecipeIngredient } = require('../models');
-const helper = require('./util')
 
 /************************************************
  * Unsecured

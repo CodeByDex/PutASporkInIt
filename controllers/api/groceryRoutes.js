ff4e76b96749ea4a0c0974031756d61e902cc394
Christian Smith <christiansmith@Christians-MBP.attlocal.net>
2023-05-06 17:35:41 -0400

    setup Grocery coontroller routes

--- controllers/api/groceryRoutes.js
+++ controllers/api/groceryRoutes.js
@@ -3,23 +3,29 @@ const {Grocery} = require('../../models');
 const helper = require('../util');
 
 router.post('/', (req, res) => {
-
+helper.SafeCreate(res, Grocery, {
+    totalAmount: req.body.totalAmount,
+    UOM: req.body.UOM
+})
 });
 
 router.get('/', (req, res) => {
-
+helper.SafeGetAll(res, Grocery, [])
 });
 
 router.get('/:id', (req, res) => {
-
+helper.SafeGetByID(req.params.id, res, Grocery, [])
 });
 
 router.put('/:id', (req, res) => {
-
+helper.SafeUpdate(res, Grocery, {
+    totalAmount: req.body.totalAmount,
+    UOM: req.body.UOM
+})
 });
 
 router.delete('/:id', (req, res) => {
-
+helper.SafeDelete(req.params.id, res, Grocery)
 });
 
 module.exports = router;

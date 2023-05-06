ec5e078fb96baab3a4301d58b24a8f0b7af2575e
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-05-06 14:12:21 -0400

    implemented locals middleware

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,25 @@
 window.addEventListener("load", () => {
-    document.querySelector("#logout").addEventListener("click", async () =>{
-        document.location.replace('/login');
-        
-        const response = await fetch('./api/users/logout', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-        });
-        
-        if (response.ok) {
-        // If successfully logged out, redirect to the login page
-        document.location.replace('/login')
-        
-        } else {
-        alert(response.statusText);
-        }
-    })
-})
+    const btnLogout = document.querySelector("#logout");
 
-
-window.addEventListener("load", () => {
-    document.querySelector("#homePage").addEventListener("click", async () =>{
-        document.location.replace('/');
-    })
-})
-
-window.addEventListener("load", () => {
-    document.querySelector("#home").addEventListener("click", async () =>{
-        document.location.replace('/');
-    })
-})
-
-window.addEventListener("load", () => {
-    document.querySelector("#browse").addEventListener("click", async () =>{
-        document.location.replace('/browse');
-    })
-})
-
-window.addEventListener("load", () => {
-    document.querySelector("#dashBoard").addEventListener("click", async () =>{
-        document.location.replace('/dashboard');
-    })
+    if(btnLogout)
+    {
+        btnLogout.addEventListener("click", async () =>{
+            document.location.replace('/login');
+            
+            const response = await fetch('./api/users/logout', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+            });
+            
+            if (response.ok) {
+            // If successfully logged out, redirect to the login page
+            document.location.replace('/login')
+            
+            } else {
+            alert(response.statusText);
+            }
+        })
+    }
 })
 
 

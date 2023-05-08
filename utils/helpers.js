4ec392c60873d302080da4accfd777e91d72b519
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-05-07 20:13:55 -0400

    added time display helpers to use best display function

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,3 +1,5 @@
+const units = require("./Units");
+
 module.exports = {
   format_time: (date) => {
     return date.toLocaleTimeString();
@@ -12,5 +14,11 @@ module.exports = {
     } else {
       return options.inverse(this);
     }
+  },
+  DisplayTimeValue(value, uom) {
+    return units.ConvertValueToBest(value, uom).val;
+  },
+  DisplayTimeUnit(value, uom) {
+    return units.ConvertValueToBest(value, uom).singular;
   }
 };

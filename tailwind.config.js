154e48bd9e93a42e861863e14e867e92609432de
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-05-15 18:47:02 -0400

    fixed tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,handlebars}"],
+  content: ["./**/*.handlebars"],
   theme: {
     extend: {},
   },
